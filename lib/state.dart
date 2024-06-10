400e5f5e5d3e1e710e5293d32ccc71689daaa099
QuantMis <realhost02@gmail.com>
2024-06-10 19:46:01 +0800

    Fix merge conflict

--- lib/state.dart
+++ lib/state.dart
@@ -1,3 +1,10 @@
 class State {
-    
+  int stateId;
+  String name;
+  State(this.stateId, this.name);
+
+  static List<State> all() {
+    List<State> states = [];
+    return states;
+  }
 }
--- lib/zone.dart
+++ lib/zone.dart
@@ -1,3 +1,17 @@
 class Zone {
-  
+  String kod, kawasan;
+  int stateId;
+
+  Zone(this.kod, this.kawasan, this.stateId);
+
+  static List<Zone> all() {
+    List<Zone> zones = [];
+    return zones;
+  }
+
+  static List<Zone> state(int StateId) {
+    // TODO: get by stateId
+    List<Zone> zones = [];
+    return zones;
+  }
 }
