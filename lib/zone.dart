446d8138171863dc748f39192446503814a397ff
QuantMis <realhost02@gmail.com>
2024-06-10 21:07:50 +0800

    Added few tests

--- lib/zone.dart
+++ lib/zone.dart
@@ -1,17 +1,24 @@
+import 'dart:developer';
+
+import 'utils/utils.dart';
+
 class Zone {
-  String kod, kawasan;
+  String code, name;
   int stateId;
 
-  Zone(this.kod, this.kawasan, this.stateId);
+  Zone(this.code, this.name, this.stateId);
 
-  static List<Zone> all() {
+  static Future<List<Zone>> all() async {
     List<Zone> zones = [];
+    List<dynamic> data = await readJsonFile('lib/data/zones.json', 'zones');
+    for (int i = 0; i < data.length; i++) {
+      zones.add(Zone(data[i]['code'], data[i]['name'], data[i]['stateId']));
+    }
     return zones;
   }
 
-  static List<Zone> state(int StateId) {
-    // TODO: get by stateId
-    List<Zone> zones = [];
-    return zones;
+  static Future<List<Zone>> state({required int stateId}) async {
+    List<Zone> zones = await all();
+    return zones.where((zone) => zone.stateId == stateId).toList();
   }
 }
--- test/malaysia_prayer_time_test.dart
+++ test/malaysia_prayer_time_test.dart
@@ -2,7 +2,25 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:malaysia_prayer_time/malaysia_prayer_time.dart';
 
 void main() {
-  test('Declare a zone variable', () {
-    late Zone zone;
+  test('get zones', () async {
+    List<Zone> zones = await Zone.all();
+    expect(zones.length, 58);
+  });
+
+  test('get states', () async {
+    List<States> states = await States.all();
+    expect(states.length, 14);
+  });
+
+  test('get zones by stateId total equal to all zones', () async {
+    List<Zone> allZones = await Zone.all();
+    List<States> states = await States.all();
+    int totalZones = allZones.length;
+    int count = 0;
+    for (int i = 0; i < states.length; i++) {
+      List<Zone> zones = await Zone.state(stateId: states[i].stateId);
+      count += zones.length;
+    }
+    expect(count, totalZones);
   });
 }
