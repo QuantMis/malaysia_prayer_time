1e8b5df66380a3f0a31c41dadbbe09c502a3fb92
QuantMis <realhost02@gmail.com>
2024-06-14 00:09:34 +0800

    Change States/Zones data source from json file to constant string

--- lib/states.dart
+++ lib/states.dart
@@ -1,4 +1,5 @@
-import 'utils/utils.dart';
+import 'dart:convert';
+import 'utils/constant.dart';
 
 class States {
   int stateId;
@@ -7,7 +8,7 @@ class States {
 
   static Future<List<States>> all() async {
     List<States> states = [];
-    List<dynamic> data = await readJsonFile('lib/data/states.json', 'states');
+    List<dynamic> data = jsonDecode(Constant.states)['states'];
     for (int i = 0; i < data.length; i++) {
       states.add(States(data[i]['id'], data[i]['name']));
     }
--- lib/utils/utils.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:convert';
-import 'dart:io';
-
-Future<List<dynamic>> readJsonFile(String filePath, String key) async {
-  var input = await File(filePath).readAsString();
-  var map = jsonDecode(input);
-  return map[key];
-}
